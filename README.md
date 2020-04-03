74eaf65a5e2e68c662604dac0e2d42418c441e38
aenemark <aenemark@nvidia.com>
2020-04-02 23:12:55 -0700

    updated readme. created template guide. added content to index. created about page. minor css tweaks. INIT.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# project-monai.github.io
-project monai website
+# Project MONAI Website
+This repo contains source code and content for the project MONAI website found at [monai.io](http://monai.io/). You can find out more on the [Project-MONAI github](https://github.com/Project-MONAI). 
+
+## Templates and Guides
+A full set of seciton templates and guides is available for reference on the [Templates Page](http://monai.io/templates.html) using `templates.html`. The site uses responsive css layouts provided by [Purecss](https://purecss.io/), [Google Fonts](https://fonts.google.com/), and font icons by [Font Aweomse](https://fontawesome.com/icons?d=gallery&m=free).
+
+### Local Development 
+Git clone this repo and open the .html pages with a browser. For the NAV and FOOTER sections to appear requires the files to be served via `python -m http.server` or similar. 
+
+## Adding / Removing Navigation Items
+To add or remove an item, go to the `footer.html` and `nav.html` files and add or remove a corresponding `<div>` and `<li>` elements. 
+
+## Adding Pages
+Too add a page, copy the `templates.html` file and remove the uncessary sections while preserving the head, nav, footer, and script sections. Then add the new .html file link to the `nav.html` and `footer.html` pages.
+
+## Adding Page Content
+Use the `templates.html` to copy the section type you want the content to appear as, and paste it into your page. Be careful to grab the full tag. If the content requires hosted images, be sure to upload them into the `/img` folder. 
+
+
---
+++ js/main.js
@@ -0,0 +1,113 @@
+// Main.js
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": false,
+        "value_area": 900
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "edge",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/MNN-icon.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.8,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 350,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
